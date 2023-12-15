00f5233aa80abe4e674c908b64ff8b6b22dd0178
ditiss1 <153855004+ditiss1@users.noreply.github.com>
2023-12-16 02:11:14 +0530

    Update nagios.plugin.sh

--- nagios.plugin.sh
+++ nagios.plugin.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+sudo apt install unzip -y
+sudo apt install automake -y
+sudo apt install autotools-dev -y
+sudo apt install  bc -y
+sudo apt install dc -y
+sudo apt install build-essential -y
+sudo apt install gawk -y
+sudo apt install gettext -y
+sudo apt install libmcrypt-dev -y
+sudo apt install libnet-snmp-perl -y
+sudo apt install libssl-dev -y
+sudo apt install snmp -y
 sudo ./tools/setup
 sudo ./configure
 sudo make
