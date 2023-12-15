3b1a8e62dc5e2d40a04440894e7accffef71ee4d
ditiss1 <153855004+ditiss1@users.noreply.github.com>
2023-12-16 02:10:47 +0530

    Update zinstall.sh

--- zinstall.sh
+++ zinstall.sh
@@ -12,18 +12,6 @@ sudo apt install python3 -y
 sudo apt install autoconf -y
 sudo apt install make -y
 sudo apt install wget -y
-sudo apt install unzip -y
-sudo apt install automake -y
-sudo apt install autotools-dev -y
-sudo apt install  bc -y
-sudo apt install dc -y
-sudo apt install build-essential -y
-sudo apt install gawk -y
-sudo apt install gettext -y
-sudo apt install libmcrypt-dev -y
-sudo apt install libnet-snmp-perl -y
-sudo apt install libssl-dev -y
-sudo apt install snmp -y
 sudo ./configure --with-httpd-conf=/etc/apache2/sites-enabled/
 sudo make all
 sudo make install-groups-users
